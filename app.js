ed3fb05722a3651694e2096cfba7e7c7401475aa
gomashio-byte <you@example.com>
2025-07-31 03:58:44 +0900

    add router

--- app.js
+++ app.js
@@ -8,8 +8,9 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var helloRouter = require('./routes/hello');
 var notesRouter = require('./routes/notes');
-var dogRouter = require('./routes/dog');
-var catRouter = require('./routes/cat');
+// var dogRouter = require('./routes/dog');
+// var catRouter = require('./routes/cat');
+var notes_from_bRouter = require('./routes/notes_from_b');
 
 var app = express();
 
@@ -27,8 +28,9 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/hello', helloRouter);
 app.use('/notes', notesRouter);
-app.use('/dog', dogRouter);
-app.use('/cat', catRouter);
+// app.use('/dog', dogRouter);
+// app.use('/cat', catRouter);
+app.use('/notes_from_b', notes_from_bRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
